c3544ef4fa7928acf2f3b2ae6925c8da4cfb1c2b
Yahya Boujrah <yahyaboujrah@Yahyas-MacBook-Air.local>
2023-12-04 19:14:07 +0100

    first commit

---
+++ src/main/java/com/RecipeManagementApp_Backend/RecipeManagementAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.RecipeManagementApp_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagementAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagementAppBackendApplication.class, args);
+	}
+
+}
